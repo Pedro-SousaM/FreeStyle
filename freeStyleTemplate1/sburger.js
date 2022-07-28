055100662f9d5ac08b707f830982831aa397b1c4
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2022-07-28 19:20:11 -0300

    Update sburger.js

--- freeStyleTemplate1/sburger.js
+++ freeStyleTemplate1/sburger.js
@@ -3,8 +3,6 @@
 
 const menu1 = document.querySelector("#menu1")  
 const op = document.querySelectorAll(".op")
-let list = document.querySelectorAll(".scrollb")
-let focus = 
 menu1.addEventListener("mouseover", ()=>{
 
     op.forEach(op => op.style.transform="translateX(80vw)") 
